fba6adf3d8b2e88c7933f1f14d3dbc85aa461828
rizal-creator <rizalbusinnes99max@gmail.com>
2024-12-12 15:09:43 +0800

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/GreyMaster/', // Ensure this is correct, corresponding to your repository name
 })
